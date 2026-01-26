4c8a8fa2b3e5d9770b1122a9544f2a10714ccb16
Andrew Krasichkov <dev@buglloc.cc>
2026-01-27 01:02:03 +0300

    Allow to ban some qtypes (#19)

--- internal/config/configpb/gen.go
+++ internal/config/configpb/gen.go
@@ -1,5 +1,3 @@
 package configpb
 
-//go:generate protoc --go_out=. config.proto
-//go:generate mv github.com/buglloc/sly64/v2/internal/config/configpb/config.pb.go .
-//go:generate rmdir -p github.com/buglloc/sly64/v2/internal/config/configpb
+//go:generate protoc --go_out=. --go_opt=paths=source_relative config.proto dns.proto
